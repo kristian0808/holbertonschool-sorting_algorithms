dcc1995a01d0309633f4517d7270049c59fe1681
rifla93 <ralfsemi@yahoo.com>
2024-01-12 11:07:10 +0100

    sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,35 +1,72 @@
-#include "sort.h"
+#include"sort.h"
 
-void swap(int* a, int* b) {
-    int t = *a;
-    *a = *b;
-    *b = t;
+/**
+ * arrangement -rearranges the array in peculiar condition order
+ * @array: -points to the array of int
+ * @min_index: -the firt index of the array
+ * @pivot_index: -the last index of the array
+ * @size: -the size of the array
+ * Return: (division_index)
+ */
+
+int arrangement(int *array, int min_index, int pivot_index, size_t size)
+{
+	int i, j, pivot, tmp, division_index;
+
+	pivot = array[pivot_index];
+	i = min_index - 1;
+	for (j = min_index; j <= pivot_index - 1; j++)
+	{
+		if (array[j] < array[pivot_index])
+		{
+			i++;
+			if (i != j)
+			{
+				tmp = array[i];
+				array[i] = array[j];
+				array[j] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+	if (array[i + 1] > pivot)
+	{
+		tmp = array[i + 1];
+		array[i + 1] = pivot;
+		array[pivot_index] = tmp;
+		print_array(array, size);
+	}
+	division_index = i + 1;
+	return (division_index);
+}
+/**
+* lomuto_sorting -recursivly calls for fragmentation
+* @array: -points to the array of int
+* @min_index: -the firt index of the array
+* @pivot_index: -the last index of the array
+* @size: -the size of the array
+*/
+
+void lomuto_sorting(int *array, int min_index, int pivot_index, size_t size)
+{
+	int division_index;
+
+	if (min_index < pivot_index)
+	{
+		division_index = arrangement(array, min_index, pivot_index, size);
+		lomuto_sorting(array, min_index, division_index - 1, size);
+		lomuto_sorting(array, division_index + 1, pivot_index, size);
+	}
 }
 
-int partition (int arr[], int low, int high, size_t size) {
-    int pivot = arr[high];
-    int i = (low - 1);
-
-    for (int j = low; j <= high - 1; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            swap(&arr[i], &arr[j]);
-            print_array(arr, size);
-        }
-    }
-    swap(&arr[i + 1], &arr[high]);
-    print_array(arr, size);
-    return (i + 1);
-}
-
-void quickSort(int arr[], int low, int high, size_t size) {
-    if (low < high) {
-        int pi = partition(arr, low, high, size);
-        quickSort(arr, low, pi - 1, size);
-        quickSort(arr, pi + 1, high, size);
-    }
-}
-
-void quick_sort(int arr[], size_t size) {
-    quickSort(arr, 0, size - 1, size);
+/**
+ * quick_sort -applies lomutos quicksort algo
+ * @array: -points to the array
+ * @size: -holds the value of the size
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+	lomuto_sorting(array, 0, size - 1, size);
 }
